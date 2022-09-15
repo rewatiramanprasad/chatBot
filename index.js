40b83b6627a5ed65c3f8716d7ac64d9630a02716
Raman <ramanprasad.0203@gmail.com>
2022-09-15 20:20:16 +0530

    work in progress

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ const client= new Client({
  
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    console.log(qr);
+    qrcode.generate(qr, {small: true}, function (qrcode) {
+        console.log(qrcode)});
 });
 
 client.on('ready', () => {
@@ -53,7 +55,7 @@ client.on('ready', () => {
 // });
 //const Chat_id="";
 client.on('message', async message => {
-    console.log(message);
+    //console.log(message);
 	const chat_id=message.id._serialized;
     let chat = await message.getChat();
     
@@ -64,12 +66,12 @@ client.on('message', async message => {
     }else if (message.body=="Good Morning"||message.body=="gd mrng"||message.body=="Gd mrng"){
         response="Good Morning!";
     }else{ 
-        console.log("else");
+        //console.log("else");
         if(chat.isGroup==false){
         await setTimeout(()=>{
 
             client.sendMessage(message.from,"I\’m sorry I couldn\’t reply you at this time, I\’m either  busy or away from my phone. I\'ll get back to you as soon as possible.")
-        },20000);
+        },300000);
 
         }
     }
